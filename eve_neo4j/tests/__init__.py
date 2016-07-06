7fcb6fecc9a00bfda42d3b70225e3107d01f6be4
Rodrigo Rodriguez <rodrigo.rn88@gmail.com>
2016-07-06 16:10:08 -0500

    Setup db for testing

--- eve_neo4j/tests/__init__.py
+++ eve_neo4j/tests/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import eve
 import os
+import random
+import string
 
+from datetime import datetime
 from eve.tests import TestMinimal
+from py2neo import Node
 
 from eve_neo4j import Neo4j
 
@@ -19,9 +23,50 @@ class TestBaseNeo4j(TestMinimal):
             settings=self.settings_file,
             url_converters=url_converters,
             data=Neo4j)
-        self.test_client = self.app.test_client()
         self.domain = self.app.config['DOMAIN']
-
+        self.known_resource_count = 101
         self.known_resource = 'people'
         self.known_resource_url = \
             '/%s' % self.domain[self.known_resource]['url']
+        self.test_client = self.app.test_client()
+        self.setupDB()
+
+        self.empty_resource = 'empty'
+        self.empty_resource_url = '/%s' % self.empty_resource
+
+    def setupDB(self):
+        self.graph = self.app.data.driver.graph
+        self.bulk_insert()
+
+    def tearDown(self):
+        self.dropGraph()
+        del self.app
+
+    def dropGraph(self):
+        self.graph.delete_all()
+
+    def bulk_insert(self):
+        people = self.random_people(self.known_resource_count)
+        people = [Node(self.known_resource, **item) for item in people]
+        for person in people:
+            try:
+                dt = datetime.now().timestamp()
+            except AttributeError:
+                import time
+                dt = time.mktime(datetime.now().timetuple())
+            person['_created'] = dt
+            person['_updated'] = dt
+            self.graph.create(person)
+
+    def random_string(self, length=6):
+        return ''.join(random.choice(string.ascii_lowercase)
+                       for _ in range(length)).capitalize()
+
+    def random_people(self, num):
+        people = []
+        for i in range(num):
+            people.append({
+                'firstname': self.random_string(6),
+                'lastname': self.random_string(6)
+            })
+        return people
